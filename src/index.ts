035f0e394d252d5b1d27abc50887b833882fd999
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2024-10-22 19:33:47 +0100

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-// import URL from 'models/url';
+import 'dotenv/config';
 // import { nanoid, customAlphabet } from 'nanoid';
 import URL from './models/url';
-// import('nanoid')
-
-dotenv.config();
 
 const app = express();
 
@@ -32,32 +28,35 @@ app.post('/', async (request: express.Request, response: express.Response) => {
   const { url } = request.body;
 
   if (!url) {
-    return response
-      .status(400)
-      .json({ error: 'Please supply the URL to be shortened' });
+    return response.status(400).json({ error: 'No URL was provided.' });
   }
 
   // check if url is a valid url
-  const urlRegex =
+  const url_regex =
     /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
-  if (!urlRegex.test(url)) {
-    return response.status(400).json({ error: 'Please supply a valid URL' });
+  if (!url_regex.test(url)) {
+    return response.status(400).json({ error: 'Invalid URL.' });
   }
 
   // use dynamic import as nanoid doesn't support compiled commonjs module
-  const { nanoid, customAlphabet } = await import('nanoid');
+  const { customAlphabet } = await import('nanoid');
 
-  const url_id = nanoid(5);
+  const nanoid = customAlphabet(
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+    5
+  );
 
-  const urlRecord = await URL.create({
+  const url_id = nanoid();
+
+  const url_record = await URL.create({
     url_id,
     destination_url: url,
   });
 
-  console.log('urlRecord', urlRecord);
+  console.log('url_record', url_record);
 
-  return response.json({ short_url: `http://localhost:${port}/${url_id}` });
+  return response.json({ short_url: `${process.env.API_BASE_URL}/${url_id}` });
 });
 
 app.get(
@@ -65,27 +64,34 @@ app.get(
   async (request: express.Request, response: express.Response) => {
     const { id } = request.params;
 
-    const urlRecord = await URL.findOne({ url_id: id });
+    const url_record = await URL.findOne({ url_id: id });
 
-    if (!urlRecord) {
+    if (!url_record) {
       return response.status(404).json({ error: 'No URL record found.' });
     }
 
-    response.redirect(urlRecord.destination_url);
+    response.redirect(url_record.destination_url);
   }
 );
 
 app.use(
   (
     error: any,
-    request: express.Request,
+    _request: express.Request,
     response: express.Response,
-    next: express.NextFunction
+    _next: express.NextFunction
   ) => {
-    return response
-      .status(error.status || 500)
-      .json({ error: error.message, stack: error.stack });
+    return response.status(error.status || 500).json({
+      error: error.message || 'Something went wrong',
+      ...(process.env.NODE_ENV === 'development' && { stack: error.stack }),
+    });
   }
+);
+
+app.use((_, response) =>
+  response.status(404).json({
+    error: "Couldn't find this endpoint. Refer to docs for valid endpoints.",
+  })
 );
 
 app.listen(port, () =>
